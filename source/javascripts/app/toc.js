e199539dc46f7b9841aa2ce8ccd98fcdc68c4762
Kevin Adams <kevin@buttercoin.com>
2014-10-09 08:46:37 -0700

    updated jquery files to fix anchor issue

--- source/javascripts/app/toc.js
+++ source/javascripts/app/toc.js
@@ -10,12 +10,7 @@
       selectors: 'h1, h2',
       extendPage: false,
       theme: 'none',
-      smoothScroll: false,
-      showEffectSpeed: 0,
-      hideEffectSpeed: 180,
       ignoreSelector: '.toc-ignore',
-      highlightOffset: 60,
-      scrollTo: -1,
       scrollHistory: true,
       hashGenerator: function (text, element) {
         return element.prop('id');
@@ -31,14 +26,6 @@
     $(".page-wrapper").click(closeToc);
     $(".tocify-item").click(closeToc);
   };
-
-  // Hack to make already open sections to start opened,
-  // instead of displaying an ugly animation
-  function animate () {
-    setTimeout(function() {
-      toc.setOption('showEffectSpeed', 180);
-    }, 50);
-  }
 
   $(makeToc);
   $(animate);
